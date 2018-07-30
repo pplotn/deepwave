e83eb96ec09d938da5aaeb823ff32ea4776f4213
Alan Richardson <arichar@tcd.ie>
2018-07-30 16:02:33 +0100

    working on max time. Going to add survey_pad for depth before continuing.

--- deepwave/fwi.py
+++ deepwave/fwi.py
@@ -1,9 +1,9 @@
 import math
 
 
-def extract_subbatch(dataset, src_locs, rec_locs,
-                     num_superbatches, num_batches, superbatch_idx,
-                     batch_idx):
+def extract_batch(dataset,
+                  num_superbatches, num_batches, superbatch_idx,
+                  batch_idx):
     num_shots = dataset.num_shots
     superbatch_size = math.ceil(num_shots / num_superbatches)
     batch_size = math.ceil(superbatch_size / num_batches)
@@ -16,8 +16,8 @@ def extract_subbatch(dataset, src_locs, rec_locs,
     batch_end = min(batch_start + batch_size, num_shots)
     batch_slice = slice(batch_start, batch_end)
     batch_data = dataset.get_shots(batch_start, batch_end)
-    batch_src_locs = src_locs[batch_slice]
-    batch_rec_locs = rec_locs[batch_slice]
+    batch_src_locs = dataset.src_locs[batch_slice]
+    batch_rec_locs = dataset.rec_locs[batch_slice]
 
     return (torch.as_tensor(batch_data), torch.as_tensor(batch_src_locs),
             torch.as_tensor(batch_rec_locs))
@@ -50,8 +50,16 @@ def pool_model(model, dt, dx, min_cells_per_wavelength=4):
     return model_pool, dx * num_pool
 
 
+def apply_max_time(batch_data_true, src_amp,
+                   dt, src_start_time,
+                   max_time):
+    batch_data_true = batch_data_true[...:max_time]
+    max_src_amp_time = src_start_time / dt + max_time
+    src_amp = src_amp[:max_src_amp_time]
+    return batch_data_true, src_amp
+
+
 # TODO:
-# * pool src and data, and use to determine and apply model pool
 # * invert increasing time
 # * separate into train and validate datasets
 # * use validate dataset to determine config changes
@@ -60,9 +68,9 @@ def pool_model(model, dt, dx, min_cells_per_wavelength=4):
 
 
 def fwi(dataset, src_amp_init, src_start_time, model_init,
-        num_pool_data,
+        num_pool_data, num_max_time,
         num_epochs, num_superbatches, num_batches, pml_width=10,
-        survey_pad=500, lr_model=1e5, lr_src_amp=0.0001,
+        survey_pad=500.0, lr_model=1e5, lr_src_amp=0.0001,
         invert_source=True, invert_model=True):
 
     # Check if GPU is available
@@ -100,49 +108,73 @@ def fwi(dataset, src_amp_init, src_start_time, model_init,
     pml_width = torch.Tensor([0, 1, 1, 1, 0, 0]) * pml_width  # free surface
 
     # Inversion loop
-    for epoch in range(num_epochs):
-        epoch_loss = 0.0
-        for superbatch_idx in range(num_superbatches):
-            optimizer.zero_grad()
-            for batch_idx in range(num_batches):
+    for max_time_idx in range(num_max_time):
+        max_time = max_time_idx * int(dataset.num_steps / num_max_time)
+        for epoch in range(num_epochs):
+            epoch_loss = 0.0
+            for superbatch_idx in range(num_superbatches):
+                optimizer.zero_grad()
+                for batch_idx in range(num_batches):
+                    # Extract batch of data
+                    batch_data_true, batch_src_locs, batch_rec_locs = \
+                        extract_batch(dataset,
+                                      num_superbatches, num_batches,
+                                      superbatch_idx, batch_idx)
+                    epoch_loss += run_batch(dataset, src_amp, src_start_time,
+                                            model, num_pool_data,
+                                            batch_data_true,
+                                            batch_src_locs, batch_rec_locs,
+                                            max_time, device, pml_width,
+                                            survey_pad, criterion, tail)
+                optimizer.step()
+            print('Epoch:', epoch, 'Loss: ', epoch_loss)
 
-                batch_data_true, batch_src_locs, batch_rec_locs = \
-                    extract_subbatch(dataset, src_locs, rec_locs,
-                                     num_superbatches, num_batches,
-                                     superbatch_idx, batch_idx)
-
-                # Pool data, source amplitude, and model
-                batch_data_true, dt = pool_data(batch_data_true, num_pool_data,
-                                                dataset.dt)
-                src_amp_pool, _ = pool_data(src_amp.reshape(1, 1, -1),
-                                            num_pool_data, dataset.dt,
-                                            src_start_time)
-                model_pool, dx = pool_model(model, dt, dataset.dx)
-
-                # Move time to first axis of data
-                batch_data_true = batch_data_true.permute(2, 0, 1)
-
-                # Send to GPU (if available)
-                batch_data_true = batch_data_true.to(device)
-                batch_src_locs = batch_src_locs.to(device)
-                batch_rec_locs = batch_rec_locs.to(device)
-
-                # Create propagator
-                prop = deepwave.scalar.Propagator(model_pool, dx,
-                                                  pml_width=pml_width,
-                                                  survey_pad=survey_pad)
-
-                # Make a copy of the source amplitude for each shot
-                batch_src_amps = \
-                    src_amp_pool.reshape(-1, 1, 1)\
-                    .repeat(1, *batch_src_locs.shape[:2])
-
-                batch_data_pred = prop(
-                    batch_src_amps, batch_src_locs, batch_rec_locs, dt)
-                loss = criterion(*tail(batch_data_pred, batch_data_true))
-                loss.backward()
-                epoch_loss += loss.detach().item()
-            optimizer.step()
-        print('Epoch:', epoch, 'Loss: ', epoch_loss)
 
     return src_amp.detach(), model.detach()
+
+
+def run_batch(dataset, src_amp, src_start_time,
+              model, num_pool_data,
+              batch_data_true,
+              batch_src_locs, batch_rec_locs,
+              max_time, device, pml_width,
+              survey_pad, criterion, tail):
+
+    # Limit to maximum time
+    batch_data_true, src_amp = apply_max_time(batch_data_true, src_amp,
+                                              dataset.dt, src_start_time,
+                                              max_time)
+    
+    # Pool data, source amplitude, and model
+    batch_data_true, dt = pool_data(batch_data_true, num_pool_data,
+                                    dataset.dt)
+    src_amp_pool, _ = pool_data(src_amp.reshape(1, 1, -1),
+                                num_pool_data, dataset.dt,
+                                src_start_time)
+    model_pool, dx = pool_model(model, dt, dataset.dx)
+
+    # Make a copy of the source amplitude for each shot
+    batch_src_amps = \
+        src_amp_pool.reshape(-1, 1, 1)\
+        .repeat(1, *batch_src_locs.shape[:2])
+    
+    # Move time to first axis of data
+    batch_data_true = batch_data_true.permute(2, 0, 1)
+    
+    # Send to GPU (if available)
+    batch_data_true = batch_data_true.to(device)
+    batch_src_locs = batch_src_locs.to(device)
+    batch_rec_locs = batch_rec_locs.to(device)
+    
+    # Create propagator
+    prop = deepwave.scalar.Propagator(model_pool, dx,
+                                      pml_width=pml_width,
+                                      survey_pad=survey_pad)
+    
+    # Propagate and calculate loss
+    batch_data_pred = prop(
+        batch_src_amps, batch_src_locs, batch_rec_locs, dt)
+    loss = criterion(*tail(batch_data_pred, batch_data_true))
+    loss.backward()
+    return loss.detach().item()
+
